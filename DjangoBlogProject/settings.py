4db26c527d5f314d01a2692f8a08aa26c54ab8d5
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-10-20 19:16:08 +0530

    render added to allowed hosts

--- DjangoBlogProject/settings.py
+++ DjangoBlogProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-(huyq7@auwg&_gexl%230v!$cr6+3mkqmiiyed51k9lh&+z2a&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
